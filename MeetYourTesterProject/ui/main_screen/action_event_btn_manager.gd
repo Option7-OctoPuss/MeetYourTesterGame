c5bc611506d8b77c03c915c5976127f9d3d23d5a
filippo <fsiri2000@gmail.com>
2023-12-19 16:24:13 +0100

    feat: implemented action event

---
+++ MeetYourTesterProject/ui/main_screen/action_event_btn_manager.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+var state_disabled = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	print("action event is timeout")
+	state_disabled = !state_disabled
+	self.set_disabled(state_disabled)
+
