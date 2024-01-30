c8223be44d9b76abc4a29872c5c3b0e125eadef8
Matteo <matteo.lalicata00@gmail.com>
2024-01-30 21:47:03 +0100

    fix: moved script anonymity_value from richtext to control node

--- MeetYourTesterProject/ui/main_screen/anonymity_value.gd
+++ MeetYourTesterProject/ui/main_screen/anonymity_value.gd
@@ -1,17 +1,19 @@
-extends RichTextLabel
+extends Control
  
-@onready var terminal = $"../../Terminal/_terminal_mock/terminal_content"
+@onready var terminal = $"../Terminal/_terminal_mock/terminal_content"
  
 const ANONYMITY_BAR_DICTIONARY_KEY = "anon_bar"
  
 func _ready():
-	terminal.connect("answer_signal", _handle_answer_signal)
+	terminal.connect("answer_signal", handle_answer_signal)
  
-func _handle_answer_signal(answer_impact: Dictionary):
+func handle_answer_signal(answer_impact: Dictionary):
 	# check if the signal is for anonimity bar
 	if ANONYMITY_BAR_DICTIONARY_KEY == answer_impact["type"]:
 		# expose value to parse it globally
 		Globals.current_anonymity_value += answer_impact["effects"]["value"]
-		self.set_text("Anonimity: %s"% Globals.current_anonymity_value)
+		
+		# TODO: replace set_text with logic to update anonymity bar
+		self.get_child(0).set_text("Anonimity: %s" % Globals.current_anonymity_value)
 		if Globals.current_anonymity_value > Globals.anonymity_value_alert_threshold:
 			print("Anonymity bar threshold passed")
