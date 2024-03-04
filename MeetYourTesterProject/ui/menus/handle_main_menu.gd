d376b00b8b78f55528dde233d5498064ca2139bd
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2024-03-04 17:03:20 +0100

    the hover and select functionality for exit button on main menu

--- MeetYourTesterProject/ui/menus/handle_main_menu.gd
+++ MeetYourTesterProject/ui/menus/handle_main_menu.gd
@@ -1,14 +1,20 @@
 extends Node
 var startIcon : TextureButton
 var startLabel : TextureButton
+
+var quitIcon : TextureButton
+var quitLabel : TextureButton
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	startIcon = $GridContainer/StartIcon
 	startLabel = $GridContainer/CenterStartLabel/StartLabel
-
+	quitIcon = $GridContainer/QuitIcon
+	quitLabel = $GridContainer/CenterQuitLabel/QuitLabel
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+# for the start icon and lable 
 
 func _on_start_game_label_pressed():
 	_debug_print("Start Game Button (label) pressed")
@@ -36,7 +42,40 @@ func _on_start_icon_mouse_entered():
 func _on_start_icon_mouse_exited():
 	_debug_print("Start Game Button (icon) on hover exited")
 	startLabel.texture_normal = ResourceLoader.load("res://images/start-scene/btn-label-start.svg")
+
+#for the quit icon and label
+
+func _on_quit_label_pressed():
+	_debug_print("Quit Game Button (label) pressed")
+	# TODO instead of changing scene, we can render it on top of the current one, but 
+	# it requires logic to disable other buttons
+	#var layer = CanvasLayer.new()
+	#add_child(layer)
+	#var scene = load("res://ui/menus/difficulty/diff_scene.tscn").instantiate()
+	#layer.add_child(scene)
+	#scene.show()
+	get_tree().change_scene_to_file("res://ui/menus/exit_menu_scene.tscn")
 	
+
+func _on_quit_icon_mouse_entered():
+	_debug_print("Quit Game Button (icon) on hover entered")
+	quitLabel.texture_normal = ResourceLoader.load("res://images/start-scene/btn-label-quit-select.svg")
+	 
+func _on_quit_icon_mouse_exited():
+	_debug_print("Quit Game Button (icon) on hover exited")
+	quitLabel.texture_normal = ResourceLoader.load("res://images/start-scene/btn-label-quit.svg")
+
+
+func _on_quit_label_mouse_entered():
+	_debug_print("Quit Game Button (label) on hover entered")
+	quitIcon.texture_normal = ResourceLoader.load("res://images/start-scene/btn-icon-exit-select.svg")
+	
+
+func _on_quit_label_mouse_exited():
+	_debug_print("Quit Game Button (label) on hover exited")
+	quitIcon.texture_normal = ResourceLoader.load("res://images/start-scene/btn-icon-exit.svg")
+	
+
 func _debug_print(msg):
 	if Globals.DEBUG_MODE:
 		print(msg)
