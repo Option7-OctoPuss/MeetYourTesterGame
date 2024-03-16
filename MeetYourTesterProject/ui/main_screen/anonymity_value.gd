ad7475e4ca34c6ccf91a2278e32e81b17d116096
rvale00 <valeria.rebagliati34@gmail.com>
2024-03-16 16:06:34 +0100

    fix anonymity bar

--- MeetYourTesterProject/ui/main_screen/anonymity_value.gd
+++ MeetYourTesterProject/ui/main_screen/anonymity_value.gd
@@ -13,8 +13,11 @@ func handle_answer_signal(selected_answer: Dictionary):
 		add_anonymity_value(selected_answer[Globals.ANONYMITY_BAR_DICTIONARY_KEY].value)
 		if Globals.current_anonymity_value > Globals.anonymity_value_alert_threshold:
 			print("Anonymity bar threshold passed")
+		
 
 func add_anonymity_value(value: int):
 	Globals.current_anonymity_value += value
+	if Globals.current_anonymity_value > Globals.max_anonimity_value:
+		Globals.current_anonymity_value = Globals.max_anonimity_value	
 	self.get_child(0).set_text("Anonimity: %s" % Globals.current_anonymity_value)
 	anon_value_update.emit()
