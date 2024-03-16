2c34740c9d4698f0782facda7d5245dcc4ab70a4
rvale00 <valeria.rebagliati34@gmail.com>
2024-03-16 16:41:27 +0100

    fix end game scene

--- MeetYourTesterProject/ui/menus/end_game_scene.gd
+++ MeetYourTesterProject/ui/menus/end_game_scene.gd
@@ -30,3 +30,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_restart_game_button_pressed():
+	get_tree().change_scene_to_file("res://ui/main_screen/main_game_scene.tscn")
+
+func _on_back_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://ui/menus/main_menu.tscn")
