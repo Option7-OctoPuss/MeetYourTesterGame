4615db9c1e8c54d776e7ba9920076845f5ce582f
ilmercu <mercuri.ndr@gmail.com>
2024-03-19 22:02:09 +0100

    implemented tutorial screens

--- MeetYourTesterProject/ui/menus/tutorial_scene.gd
+++ MeetYourTesterProject/ui/menus/tutorial_scene.gd
@@ -1,22 +1,19 @@
 extends Node2D
 
-var current_tutorial_screen = "global_timing"
+var current_tutorial_screen_idx = 0;
 var speed = 16
 var current_step = 3
 var tutorial_file_path = "res://static-data/tutorial-content/tutorial.json"
 var tutorials_content = null
 var tutorial_current_text = ""
-var current_idx_text = 0
 var current_tutorial_content_length = 0
-var arrow_position = null
 var current_tutorial_content = null
 
 func _ready():
 	tutorials_content = JSON.parse_string(FileAccess.get_file_as_string(tutorial_file_path))
-	current_tutorial_content_length = len(tutorials_content[current_tutorial_screen])
-	current_tutorial_content = tutorials_content[current_tutorial_screen][current_idx_text]
+	var current_tutorial_screen_key = tutorials_content.keys()[current_tutorial_screen_idx]
+	current_tutorial_content_length = len(tutorials_content[current_tutorial_screen_key])
 	change_tutorial_data()
-
 
 func _physics_process(delta: float) -> void:
 	var highlightSprite: Sprite2D = find_child("HighlightIcon")
@@ -29,27 +26,35 @@ func _physics_process(delta: float) -> void:
 		highlightSprite.position.y -= speed * delta
 
 	current_step -= 1
-	
 
-func _on_previous_pressed():
-	if current_idx_text > 0:	
-		current_idx_text -= 1
-		change_tutorial_data()
-
-
-func _on_next_pressed():
-	current_idx_text += 1
-	
-	if current_idx_text >= current_tutorial_content_length:
-		print("Passa alla scena dopo")
-		current_idx_text = 0
+func change_scene(offset: int=1):
+	if current_tutorial_screen_idx <= 0 and offset < 0:
 		return
 	
+	if current_tutorial_screen_idx >= len(tutorials_content.keys())-1 and offset > 0:
+		return
+	
+	# hide the current scene
+	find_child(tutorials_content.keys()[current_tutorial_screen_idx]).visible = false
+	current_tutorial_screen_idx += offset
+	
+	# show new scene
+	find_child(tutorials_content.keys()[current_tutorial_screen_idx]).visible = true
+	
 	change_tutorial_data()
+
+func _on_previous_pressed():
+	change_scene(-1)
+
+func _on_next_pressed():
+	change_scene()
 	
 func change_tutorial_data() -> void:
-	current_tutorial_content = tutorials_content[current_tutorial_screen][current_idx_text]
+	var current_tutorial_screen_key = tutorials_content.keys()[current_tutorial_screen_idx]
+	current_tutorial_content = tutorials_content[current_tutorial_screen_key]
 	tutorial_current_text = current_tutorial_content.text
-	arrow_position = Vector2(current_tutorial_content.arrow_pos.x, current_tutorial_content.arrow_pos.y)
-	find_child(current_tutorial_screen).find_child("TutorialBody").set_text(tutorial_current_text)
-	find_child("HighlightIcon").position = arrow_position
+	$HighlightIcon.position = Vector2(current_tutorial_content.arrow_pos.x, current_tutorial_content.arrow_pos.y)
+	#find_child(tutorials_content.keys()[current_tutorial_screen_idx]).find_child("TutorialBody").set_text(tutorial_current_text)
+	$Popup/TutorialBody.set_text(tutorial_current_text)
+	$Popup.scale = Vector2(tutorials_content[current_tutorial_screen_key]["popup"]["scale"], tutorials_content[current_tutorial_screen_key]["popup"]["scale"])
+	$Popup.position = Vector2(tutorials_content[current_tutorial_screen_key]["popup"]["pos"]["x"], tutorials_content[current_tutorial_screen_key]["popup"]["pos"]["y"])
